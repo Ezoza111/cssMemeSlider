709d735edf50e437a532560b67ae257f6e221be9
ezoza <ezozakhudayberdieva@mail.ru>
2023-09-14 11:34:16 +0300

    feat: add animation for slaider images (Thu, 14 Sep, 2023, 11:33:43AM)

--- cssMemeSlider/index.js
+++ cssMemeSlider/index.js
@@ -1,12 +1,17 @@
 const image = document.querySelector('.slaider_foto');
 const btnArr = Array.from(document.querySelectorAll('.button_pogination'));
+const btnContentArr = Array.from(document.querySelectorAll('.btn_div'));
 const text = document.querySelector('.text_container');
-const textInfo = ['Большая галактика', 'Супер большая галактика','Оооочень большая галактика','Жесть как сложно учиться','Мега большая галактика']
+const textInfo = ['Большая галактика', 'Супер большая галактика','Оооочень большая галактика','Жесть как сложно учиться','Мега большая галактика'];
 
 
 btnArr.forEach((el)=> el.addEventListener('click', ()=>{
-    
-    image.setAttribute('src', `./assets/foto/photo_${btnArr.indexOf(el)+1}.jpg`);
-
-    text.innerHTML = `<div class="text_animation">${textInfo[btnArr.indexOf(el)]}</div>}`;
+    image.setAttribute('src', `./assets/foto/photo_${btnArr.indexOf(el)+1}.jpg`)
+    image.classList.add('img_animation');
+    image.addEventListener('animationend', ()=> {if(image.classList.contains('img_animation')==true){image.classList.remove('img_animation')}});
+    text.innerHTML = `<div class="text_animation">${textInfo[btnArr.indexOf(el)]}</div>`;
+    for(let key in btnContentArr) {
+    btnContentArr[btnArr.indexOf(el)].classList.add('active');
+    if(key != btnArr.indexOf(el)){btnContentArr[key].classList.remove('active');}
+    }
 }));
